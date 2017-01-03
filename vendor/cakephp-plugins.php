630f332b26692002678b7c9084794a04999b651f
Vuong <vulgoplus@gmail.com>
2017-01-04 06:34:35 +0700

    Add missing files

---
+++ vendor/cakephp-plugins.php
@@ -0,0 +1,12 @@
+<?php
+$baseDir = dirname(dirname(__FILE__));
+return [
+    'plugins' => [
+        'Bake' => $baseDir . '/vendor/cakephp/bake/',
+        'DebugKit' => $baseDir . '/vendor/cakephp/debug_kit/',
+        'Josegonzalez/Upload' => $baseDir . '/vendor/josegonzalez/cakephp-upload/',
+        'Migrations' => $baseDir . '/vendor/cakephp/migrations/',
+        'Proffer' => $baseDir . '/vendor/davidyell/proffer/',
+        'Utils' => $baseDir . '/vendor/cakemanager/cakephp-utils/'
+    ]
+];
